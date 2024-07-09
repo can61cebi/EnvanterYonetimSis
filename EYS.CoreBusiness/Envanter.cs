51f6690e7d1259808cb047c1540236b0cc8c3106
Can Çebi <can61cebi@gmail.com>
2024-07-09 10:54:36 +0300

    düzenleme: envanter ekleme değer kontrolü yapıldı

--- EYS.CoreBusiness/Envanter.cs
+++ EYS.CoreBusiness/Envanter.cs
@@ -1,10 +1,19 @@
-﻿namespace EYS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EYS.CoreBusiness
 {
     public class Envanter
     {
         public int EnvanterId { get; set; }
+
+        [Required(ErrorMessage = "Envanter ismi girilmelidir.")]
+        [StringLength(130, ErrorMessage = "Envanter ismi en fazla 130 karakter olmalıdır.")]
         public string EnvanterIsim { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Adet 0 ya da daha büyük olmalıdır.")]
         public int Adet {  get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Fiyat 0 ya da daha büyük olmalıdır.")]
         public double Fiyat { get; set; }
     }
 }
