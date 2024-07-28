8f3dcfba91bc1b79a089d58c44154f7267819ab8
Can Çebi <can61cebi@gmail.com>
2024-07-28 19:35:37 +0300

    ekleme: envanter satın alma sayfası tamamlandı

---
+++ EYS.WebApp/ViewModels/SatinAlmaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EYS.WebApp.ViewModels
+{
+    public class SatinAlmaViewModel
+    {
+        [Required]
+        public string almaSayisi { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage ="Bir envanter seçmelisiniz.")]
+        public int EnvanterId { get; set; }
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Adet 1 veya daha fazla olmalıdır..")]
+        public int SatinAlmaAdeti { get; set; }
+        public double EnvaterFiyati { get; set; }
+
+    }
+}
